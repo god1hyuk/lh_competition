66c7d145d0efb1a3487b39732729609b71b525ce
wonhyukshin <77233579+wonhyukshin@users.noreply.github.com>
2021-09-23 18:42:13 +0900

    20210923_18:42

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,3 +1,88 @@
+// 인트로 페이지
+const loadingPage = document.querySelector('.loading_page');
+const introLogo = document.querySelector('.loading_page .logo');
+
+setTimeout(() => {
+    introLogo.style.transition = 'opacity 1s';
+    introLogo.style.opacity = 1;
+
+    // 타이핑 효과
+    let typingBool = false;
+    let typingIndex = 0;
+    let typingText = document.querySelector('.typing_text').textContent;
+    const typingFx = document.querySelector('.typing');
+    typingText = typingText.split(''); 
+
+    setTimeout(() => {
+        if (typingBool == false) {
+            typingBool = true;
+            var typingInt = setInterval(typing, 120);
+        }
+
+        function typing () {
+            if (typingIndex < typingText.length) {
+                typingFx.append(typingText[typingIndex]);
+                typingIndex++;
+            } else if (typingIndex == typingText.length) {
+                clearInterval(typingInt);
+                setTimeout(() => {
+                    makeLoader(101, loader, '#f27214');
+                    loader.style.opacity = 1;
+                }, 500);
+            }
+        }
+
+        // 로더
+        const loader = document.querySelector('.loader');
+        const progressNum = document.querySelector('.loader .inner');
+        loader.style.transition = 'opacity .5s';
+        loader.style.opacity = 0;
+
+        const makeLoader = (percent, classname, color) => {
+            let i = 0;
+            let loaderFn = setInterval(function () {
+                if (i < percent) {
+                    colorFn(i, classname, color);
+                    progressNum.textContent = i + " %";
+                    i++;
+                } else {
+                    clearInterval(loaderFn);
+                    setTimeout(() => {
+                        loadingPage.style.transition = 'opacity 1s';
+                        loadingPage.style.opacity = 0;
+                    }, 500);
+                    setTimeout(() => {
+                        location.href = 'index.html';
+                    }, 2000);
+                }
+            }, 50);
+        }
+
+        const colorFn = (i, classname, color) => {
+            classname.style.background = 'conic-gradient(' + color + ' 0%' + i + '%, #ececec ' + i + '% 100%)';
+        }
+
+    }, 1000);
+
+}, 1000);
+
+// 팝업
+const modal = document.querySelectorAll('.modal_bg');
+const modal_on = document.querySelectorAll('#modal_on');
+const modal_close = document.querySelectorAll('.modal_close');
+
+
+for (let i=0; i<modal.length; i++) {
+    // modal[i].style.display = 'none';
+    modal_on[i].addEventListener('click', function () {
+        modal[i].classList.add('active');
+    });
+    modal_close[i].addEventListener('click', function () {
+        modal[i].classList.remove('active');
+    });
+}
+
+// 메인 슬라이더
 const sliderWrapper = document.querySelector('.slider_wrapper');
 const itemAll = document.querySelectorAll('.slider_item');
 const itemMarginLeft = Number(parseFloat(window.getComputedStyle(itemAll[0]).getPropertyValue('margin-left')));
@@ -7,6 +92,7 @@ console.log(curIndex);
 
 const sliderPrev = document.querySelector('#sliderPrev');
 const sliderNext = document.querySelector('#sliderNext');
+const pagination = document.querySelectorAll('.sPagination span');
 const clonedFirst = itemAll[0].cloneNode(true);
 const clonedSecond = itemAll[1].cloneNode(true);
 const clonedLast = itemAll[itemAll.length-1].cloneNode(true);
@@ -29,7 +115,12 @@ console.log(sliderItem[curIndex + 1]);
 sliderNext.addEventListener('click', function () {
     curIndex++;
 
-    // console.log(itemAll[curIndex]);
+    for (let i=0; i<pagination.length; i++) {
+        pagination[i].classList.remove('active');
+        pagination[i].style.transition = 'background .13s';
+    }
+    pagination[curIndex].classList.add('active');
+
     console.log(sliderItem[curIndex + 1]);
 
     sliderWrapper.style.transition = 'right 1.2s';
@@ -42,6 +133,7 @@ sliderNext.addEventListener('click', function () {
     if (curIndex > itemAll.length-2) {
         setTimeout(function () {
             curIndex = -1;
+
             console.log(curIndex);
             for (item in sliderItem) {
                 sliderItem[item].style.transition = 'all 0s';
@@ -60,6 +152,14 @@ sliderNext.addEventListener('click', function () {
 sliderPrev.addEventListener('click', function () {
     curIndex--;
 
+    for (let i=0; i<pagination.length; i++) {
+        pagination[i].classList.remove('active');
+        pagination[i].style.transition = 'background .13s';
+    }
+    if (curIndex >= 0) {
+        pagination[curIndex].classList.add('active');
+    }
+
     console.log(sliderItem[curIndex + 1]);
 
     sliderWrapper.style.transition = 'right 1.2s';
@@ -72,8 +172,12 @@ sliderPrev.addEventListener('click', function () {
     sliderItem[curIndex+2].style.transform = 'translateY(11.0878vw)';
     
     if (curIndex < 0) {
+
+        pagination[pagination.length-1].classList.add('active');
+
         setTimeout(function () {
             curIndex = itemAll.length-1;
+            
             console.log(curIndex);
             for (item in sliderItem) {
                 sliderItem[item].style.transform = 'translateY(0)';
