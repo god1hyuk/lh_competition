f017352c0f05fc56dd16a3319de01af7b78c25ad
wonhyukshin <77233579+wonhyukshin@users.noreply.github.com>
2021-09-29 18:09:43 +0900

    20210929_18:09

---
+++ asset/js/main2.js
@@ -0,0 +1,47 @@
+
+// 메인 페이지
+
+// 메인 슬라이더
+$(document).ready(function () {
+    const sliderWrapper = $('.main_slider .slider_wrapper');
+    const sliderItem = $('.main_slider .slider_item');
+    const sliderItem_width = sliderItem[0].clientWidth;
+    const itemMarginLeft = Number(parseFloat(window.getComputedStyle(sliderItem[0]).getPropertyValue('margin-left')));
+    const clonedFirst = sliderItem[0].cloneNode(true);
+    const clonedLast = sliderItem[sliderItem.length-1].cloneNode(true);
+    sliderWrapper.append(clonedFirst);
+    sliderWrapper.prepend(clonedLast);
+    
+    let slideCount = 0;
+    console.log(slideCount);
+    sliderWrapper.css('right', -(sliderItem_width + itemMarginLeft) * (slideCount + 1) + 'px');
+
+    $('.main_slider #sliderNext').on('click', function () {
+        slideCount++;
+        console.log(slideCount);
+        sliderWrapper.css('transition', 'right 1.2s');
+        sliderWrapper.css('right', -(sliderItem_width + itemMarginLeft) * (slideCount + 1) + 'px');
+        if (slideCount > sliderItem.length -2) {
+            setTimeout(function () {
+                slideCount = -1;
+                sliderWrapper.css('transition', '0s');
+                sliderWrapper.css('right', -(sliderItem_width + itemMarginLeft) * (slideCount + 1) + 'px');
+                console.log(slideCount);
+            }, 1200);
+        }
+    });
+    $('.main_slider #sliderPrev').on('click', function () {
+        slideCount--;
+        console.log(slideCount);
+        sliderWrapper.css('transition', 'right 1.2s');
+        sliderWrapper.css('right', -(sliderItem_width + itemMarginLeft) * (slideCount + 1) + 'px');
+        if (slideCount < 0) {
+            setTimeout(function () {
+                slideCount = 4;
+                sliderWrapper.css('transition', '0s');
+                sliderWrapper.css('right', -(sliderItem_width + itemMarginLeft) * (slideCount + 1) + 'px');
+                console.log(slideCount);
+            }, 1200);
+        }
+    });
+});
