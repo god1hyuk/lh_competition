d1072f82a4c5fc5c2a4f543608936c9b8793e866
wonhyukshin <77233579+wonhyukshin@users.noreply.github.com>
2021-09-30 16:23:48 +0900

    20210930_16:23

--- asset/js/main2.js
+++ asset/js/main2.js
@@ -15,17 +15,20 @@ $(document).ready(function () {
     let slideCount = 0;
     console.log(slideCount);
     sliderWrapper.css('right', -(sliderItem_width + itemMarginLeft) * (slideCount + 1) + 'px');
+    sliderItem[0].style.transform = 'translateY(0)';
 
     $('.main_slider #sliderNext').on('click', function () {
         slideCount++;
         console.log(slideCount);
         sliderWrapper.css('transition', 'right 1.2s');
         sliderWrapper.css('right', -(sliderItem_width + itemMarginLeft) * (slideCount + 1) + 'px');
-        if (slideCount > sliderItem.length -2) {
+        sliderItem[slideCount].style.transform = 'translateY(0)';
+        if(slideCount > sliderItem.length -2) {
             setTimeout(function () {
                 slideCount = -1;
                 sliderWrapper.css('transition', '0s');
                 sliderWrapper.css('right', -(sliderItem_width + itemMarginLeft) * (slideCount + 1) + 'px');
+                $('.main_slider .slider_item').css('transform', 'translateY(10.9896vw)');
                 console.log(slideCount);
             }, 1200);
         }
@@ -35,9 +38,11 @@ $(document).ready(function () {
         console.log(slideCount);
         sliderWrapper.css('transition', 'right 1.2s');
         sliderWrapper.css('right', -(sliderItem_width + itemMarginLeft) * (slideCount + 1) + 'px');
-        if (slideCount < 0) {
+        sliderItem[slideCount+1].style.transform = 'translateY(10.9896vw)';
+        if(slideCount < 0) {
             setTimeout(function () {
                 slideCount = 4;
+                sliderItem[0].style.transform = 'translateY(0)';
                 sliderWrapper.css('transition', '0s');
                 sliderWrapper.css('right', -(sliderItem_width + itemMarginLeft) * (slideCount + 1) + 'px');
                 console.log(slideCount);
